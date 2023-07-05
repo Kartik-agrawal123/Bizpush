afc15d782cc674d49365535aa5f95a6503baf649
Kartik Agrawal <kartikagrawal4321@gmail.com>
2023-07-05 18:34:23 +0530

    Add Popular Product component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
+import PopularProduct from "./components/PopularProduct";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div className="bg-gradient-to-r from-customRed to-customBlue">
         <NavBar />
         <Hero />
+        <PopularProduct/>
       </div>
     </>
   );
